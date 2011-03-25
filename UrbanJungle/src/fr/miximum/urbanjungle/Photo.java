a04ebad768d29dd9b12d23b60d3d878b010cc257
Thibault Jouannic <teeboo@gmail.com>
2011-03-25 11:33:33 +0100

    Initial commit

---
+++ UrbanJungle/src/fr/miximum/urbanjungle/Photo.java
@@ -0,0 +1,15 @@
+package fr.miximum.urbanjungle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Photo extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
