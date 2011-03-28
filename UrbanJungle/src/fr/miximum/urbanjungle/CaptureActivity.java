85c7b8b0b0e062ea8c8231aa2afcbeacdcf65c26
Thibault Jouannic <teeboo@gmail.com>
2011-03-28 15:48:15 +0200

    use phone camera app to store pictures

--- UrbanJungle/src/fr/miximum/urbanjungle/CaptureActivity.java
+++ UrbanJungle/src/fr/miximum/urbanjungle/CaptureActivity.java
@@ -19,12 +19,28 @@
 package fr.miximum.urbanjungle;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+
+import java.io.File;
 
 public class CaptureActivity extends Activity
 {
+    /** Tag used for log */
      static final String TAG="UrbanJungle";
+
+     /** The captured photo file name */
+     private static final String CAPTURE_TITLE="UrbanJungle.jpg";
+
+     /** Arbitrary code to use with getActivityForResult */
+     private static final int TAKE_PHOTO_CODE = 2;
 
     /** Called when the activity is first created. */
     @Override
@@ -34,5 +50,29 @@ public class CaptureActivity extends Activity
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.capture);
+
+        Button capture = (Button) findViewById(R.id.capture_button);
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // We use the stock camera app to take a photo
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                // Store image in dcim
+                File file = new File(Environment.getExternalStorageDirectory() + "/DCIM", CAPTURE_TITLE);
+                Uri imgUri = Uri.fromFile(file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+                startActivityForResult(intent, TAKE_PHOTO_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+        }
     }
 }
