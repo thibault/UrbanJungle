f40a58e6dfa3032320a19a94bc1972676ea3aec9
Thibault Jouannic <teeboo@gmail.com>
2011-03-29 15:16:17 +0200

    upload code

--- UrbanJungle/src/fr/miximum/urbanjungle/HttpUploader.java
+++ UrbanJungle/src/fr/miximum/urbanjungle/HttpUploader.java
@@ -19,24 +19,28 @@
 
 package fr.miximum.urbanjungle;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Service;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.IBinder;
 import android.util.Log;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class HttpUploader extends Service {
 
-    private static final String UPLOAD_URL = "http://thibault-laptop:5000/upload";
+    private static final String UPLOAD_URL = "http://thibault-laptop:8080/upload";
+
+    private static final String FORM_FILE_TITLE = "file";
 
     @Override
     public void onStart(Intent intent, int startId) {
@@ -60,82 +64,22 @@ public class HttpUploader extends Service {
      * @param file
      * @param uploadUrl
      */
-    private void doFileUpload(File file, String uploadUrl) {
-        HttpURLConnection conn = null;
-        DataOutputStream dos = null;
-        DataInputStream inStream = null;
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        int maxBufferSize = 1 * 1024 * 1024;
-        String serverOutput = "";
+    private boolean doFileUpload(File file, String uploadUrl) {
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(uploadUrl);
+        MultipartEntity entity = new MultipartEntity();
+        entity.addPart(FORM_FILE_TITLE, new FileBody(file));
+        post.setEntity(entity);
+        HttpResponse response = null;
+
         try {
-
-            // Open url
-            FileInputStream fileInputStream = new FileInputStream(file);
-            URL url = new URL(uploadUrl);
-
-            // Configure connection
-            conn = (HttpURLConnection) url.openConnection();
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
-            conn.setUseCaches(false);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-
-            // Start request
-            dos = new DataOutputStream(conn.getOutputStream());
-            dos.writeBytes(twoHyphens + boundary + lineEnd);
-            dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\""
-                    + file.getName() + "\"" + lineEnd);
-            dos.writeBytes(lineEnd);
-
-            // create a buffer of maximum size
-            bytesAvailable = fileInputStream.available();
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-
-            // read file and write it into form.
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            while (bytesRead > 0) {
-                dos.write(buffer, 0, bufferSize);
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            }
-
-            // send multipart form data necesssary after file data...
-            dos.writeBytes(lineEnd);
-            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-            // close streams
-            Log.e(CaptureActivity.TAG, "File is written");
-            fileInputStream.close();
-            dos.flush();
-            dos.close();
-        } catch (MalformedURLException ex) {
-            Log.e(CaptureActivity.TAG, "error: " + ex.getMessage(), ex);
-        } catch (IOException ioe) {
-            Log.e(CaptureActivity.TAG, "error: " + ioe.getMessage(), ioe);
+            response = client.execute(post);
+        } catch (ClientProtocolException e) {
+            Log.e(CaptureActivity.TAG, "Protocol exception " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(CaptureActivity.TAG, "IO exception " + e.getMessage());
         }
 
-        // Server response
-        try {
-            inStream = new DataInputStream(conn.getInputStream());
-            String str;
-
-            while ((str = inStream.readLine()) != null) {
-                serverOutput += str;
-            }
-            inStream.close();
-
-        } catch (IOException ioex) {
-            Log.e(CaptureActivity.TAG, "error: " + ioex.getMessage(), ioex);
-        }
-
-        Log.d(CaptureActivity.TAG, "Server output : " + serverOutput);
+        return response.getStatusLine().getStatusCode() == 200;
     }
 }
