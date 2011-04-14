db0451d99da58ac00086e6fe7399e356fb660173
Thibault Jouannic <teeboo@gmail.com>
2011-04-14 13:57:11 +0200

    Well, I guess we won't use sqlalchemy-migrate

--- site/dbmanage.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from migrate.versioning.shell import main
-from urbanjungle import app
-
-
-main(url=app.config['SQLALCHEMY_DATABASE_URI'], debug=app.config['DEBUG'], repository='migrations/')
